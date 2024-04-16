97f8d20dcb792831a62b02e23e4859dfa0df6e1a
saurabhbisht95 <160516320+saurabhbisht95@users.noreply.github.com>
2024-04-16 20:41:35 +0530

    Update CustomLL2.java

--- LinkedList/CustomLL2.java
+++ LinkedList/CustomLL2.java
@@ -1,3 +1,4 @@
+//Note :- Here i used the tail part which is not necessary to use the tail part while doing deletion in linkedlist, but for beginners i recommend to use the tail for better understanding.
 package LinkedLIst;
 
 public class CustomLL2 {
